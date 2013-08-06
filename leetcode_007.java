36363336a940d0c867cb869e92060272f5dc4fc9
daianqi <daianqi@umich.edu>
2013-08-06 02:37:13 -0400

    Reverse Integer

---
+++ leetcode_007.java
@@ -0,0 +1,23 @@
+/*
+Reverse Integer.
+Reverse digits of an integer.
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+*/
+
+public class Solution {
+    public int reverse(int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        boolean neg = (x<0)?true:false;
+        if(neg) x*=-1;
+        int result =0;
+        while(x>0) {
+            result += x%10;
+            x /= 10;
+            if(x>0)
+                result *= 10;
+        }
+        return neg?(result*-1):result;
+    }
+}
