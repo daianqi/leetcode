f1272305848be900057e48a733cefa7de10a0587
daianqi <daianqi@umich.edu>
2013-08-28 12:02:18 -0400

    Remove Element

---
+++ leetcode_027.java
@@ -0,0 +1,19 @@
+/*
+Remove Element.
+Given an array and a value, remove all instances of that value in place and return the new length.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+*/
+
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int j = 0;
+        for(int i = 0;i<A.length;i++) {
+            if(A[i] != elem) {
+                A[j++] = A[i];
+            }
+        }
+        return j;
+    }
+}
